e8f43885415a39317d3ee584e48722d2841ca0d2
Darrel Baerwaldt <darrel.baerwaldt@hotmail.com>
2022-02-16 15:22:53 +0900

    some styling, class functionality to match races

--- components/charClasses.js
+++ components/charClasses.js
@@ -1,20 +1,34 @@
 async function getClasses() {
-  const charClass = document.getElementById("charClass");
+  const charClasses = document.getElementById("charClasses");
   const response = await fetch("https://www.dnd5eapi.co/api/classes/");
   const data = await response.json();
   console.dir(data);
-  const cClass = data.results;
+  const cClasses = data.results;
 
-  cClass.forEach((job) => {
+  cClasses.forEach((job) => {
     let tempJobHTML = `
-    <ul class="list-group" style="width: 10rem;">
-  <li class="list-group-item">
-    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+    <ul id="classes" class="list-group" style="width: 10rem;">
+  <li id="class" class="list-group-item">
+    <input id="class-box" class="class-check-box" type="checkbox" value="" aria-label="...">
     ${job.name}
   </li>
 </ul>`;
 
-    charClass.innerHTML += tempJobHTML;
+    charClasses.innerHTML += tempJobHTML;
+  });
+
+  const checkboxes = document.querySelectorAll(
+    "input[class='class-check-box']"
+  );
+
+  charClasses.addEventListener("change", (e) => {
+    e.target.parentElement.style.color = "red";
+    e.target.parentElement.style.backgroundColor = "black";
+    if ((e.target.checked = true)) {
+      checkboxes.forEach((box) => {
+        box.disabled = true;
+      });
+    }
   });
 }
 
