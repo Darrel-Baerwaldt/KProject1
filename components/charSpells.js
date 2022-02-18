de4b5499a2afa530263743611673a1168aa428da
G. Thomas Ellis Jr <gte-kingsland@hotmail.com>
2022-02-17 22:16:15 -0500

    Pulling latest

--- components/charSpells.js
+++ components/charSpells.js
@@ -1,22 +1,25 @@
 async function getSpells() {
     const charSpells = document.getElementById("charSpells");
-    const response = await fetch("https://www.dnd5eapi.co/api/spells"); 
+    const response = await fetch("https://www.dnd5eapi.co/api/spells?level=1"); 
     const data = await response.json();
-    const cRace = data.results;
-  
-    cRace.forEach((spells) => {
+    const cSpells = data.results;
+// console.log(cSpells);
+    
+    cSpells.forEach((spell) => {
+ //     console.log(spell.name);
       let tempSpellsHTML = `
       <ul class="list-group" style="width: 10rem;">
     <li class="list-group-item">
       <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
-      ${spells.name}
+      ${spell.name}
     </li>
   </ul>`;
       charSpells.innerHTML += tempSpellsHTML;
+//      console.log(spells.name);
     });
   }
-  console.log(response);
-  
+//  console.log(response);
+
   export { getSpells };
 
 
