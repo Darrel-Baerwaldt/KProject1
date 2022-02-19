6124cbc2bdf1731f4c82e7342ba2eca569a0d495
Darrel Baerwaldt <darrel.baerwaldt@hotmail.com>
2022-02-19 12:49:48 +0900

    puul before pushing splash page

--- script.js
+++ script.js
@@ -4,10 +4,29 @@ import { getRace } from "./components/charRace.js";
 import { stats } from "./components/charStats.js";
 import { getSkill } from "./components/charSkills.js";
 import { getNames } from "./components/names.js";
+import { splashPage } from "./components/splashPage.js";
+
+splashPage();
+
+// const splash = document.querySelector(".splash");
+
+// const splashH1 = document.createElement("h1");
+// splashH1.setAttribute("class", "fade-in");
+// splashH1.innerHTML = `If you made it here, <br />you must be, <br />STILL-NERD'in!`;
+// splash.appendChild(splashH1);
+
+// document.addEventListener("DOMContentLoaded", (e) => {
+//   console.log(splash);
+//   setTimeout(() => {
+//     console.log(splash);
+//     splash.classList.add("display-none");
+//     // splash.hidden = true;
+//   }, 5000);
+// });
 
 getClasses();
 getRace();
 stats();
 getSkill();
-getNames()
+getNames();
 getMyPic();
